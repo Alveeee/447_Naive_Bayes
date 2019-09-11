d84d28216d39a3ce519adf587c31b14015e4ac8e
Alex Alvarez <doubleabatteryalvarez@gmail.com>
2019-09-10 21:50:59 -0600

    Update Learner.py

--- Learner.py
+++ Learner.py
@@ -42,6 +42,50 @@ def kFoldCross(data, splitRatio, k):
     
     return [trainSet, testSet]
 
+import csv
+import random
+
+#readCsv reads in the preprocessed data
+def readCsv(file):
+    data = csv.reader(open(file, "r"))
+    data_list = list(data)
+    for i in range(len(data_list)):
+        for j in range(len(data_list[i])):
+            data_list[i] = [float(x) for x in data_list[i]]
+    return data_list
+
+#randomize data so that when we select training and test sets, we get a variety of each class
+def randomizeData(data):
+    randomSet = []
+    copy = list(data)
+    while len(randomSet) < len(data):
+        index = random.randrange(len(copy))
+        randomSet.append(copy.pop(index))
+    return randomSet
+
+def getClasses(data):
+    classes = []
+    for i in range(len(data)):
+        if(not(data[i][len(data[i])-1] in classes)):
+           classes.append(data[i][len(data[i])-1])
+    return classes
+
+#kFoldCross is how we split the data into training and test sets using 10 fold cross validation
+def kFoldCross(data, splitRatio, k):
+    #size of test set (for 100 records and a ratio of .9, it would be 10 records)
+    testSize = int(len(data) - len(data) * splitRatio)
+    index = k*testSize
+    
+    trainSet = list(data)
+    testSet = []
+
+    for i in range(testSize):
+        testSet.append(trainSet.pop(index))
+    
+    print('Found Training Set\nFound Test Set')
+    
+    return [trainSet, testSet]
+
 #returns class-specific dataset(from given dataset) for given class [Q(C = c_i)]
 def get_class_data(data, c):
     #array for storing class-specific dataset
@@ -62,14 +106,14 @@ def get_class_data_old(dataset_name, class_identity):
     class_data = []
 
     #open original dataset
-    with open(dataset_name) as dataset:
+    with open("data/" + dataset_name) as dataset:
 
         #read data
         for line in dataset:
             features = line.split(",")
 
             #if the class matches the requested identity, add to class-specific dataset
-            if features[-1] == class_identity + "\n":
+            if features[-1] == str(class_identity) + "\n":
                 class_data.append(features)
     print(class_data)
     return class_data
@@ -92,12 +136,12 @@ def find_attribute_probability(class_data, numValues):
 
     #divide the number of examples matching each value by the number of examples in the class
     for attribute in table:
-        for i in range(numValues - 1):
+        for i in range(numValues):
             attribute[i] /= numAttributes - 1 + (len(class_data))
 
     #store sample's class size in probability table
     table[-1][0] = len(class_data)
-    
+    #print_probability(table)
     return table
 
 #returns predicted class *index* for example using learned set
@@ -110,6 +154,7 @@ def classify_example(example, learned_set):
     for c in range(len(learned_set)):
         C.append(0)
         sample_size += len(learned_set[c])
+        
         #iterate over each attribute in example
         for a in range(len(example)-1):
 
@@ -117,7 +162,7 @@ def classify_example(example, learned_set):
             C[c] += learned_set[c][a][int(example[a])-1]
 
         #multiply sum by sample's class size
-        C[c] *= int(learned_set[c][-1][0]) / sample_size
+        #C[c] *= int(learned_set[c][-1][0]) / sample_size
         print("Total for class " + str(c) + ": " + str(C[c]))
 
     #find highest value in C[] as prediction
@@ -163,6 +208,7 @@ def print_probability(probability_table):
             total += value
         print(str(total) + '\n')
 
+
 #reading in data from file
 file = "data/BCD-processed.csv"
 data = readCsv(file)
@@ -182,8 +228,24 @@ for i in range(kfold):
     for example in testSet:
         print("Predicted class: {}".format(classes[classify_example(example, learned)]))
 
-BCD_identities = ["2","4"]
-BCD_examples = ["2,1,1,1,2,1,2,1,1,2".split(","),"10,10,10,4,8,1,8,10,1,4".split(","),"1,1,1,1,2,1,3,2,1,2".split(","),"5,1,3,1,2,1,2,1,1,2".split(",")]
-#BCD_learned = learn_dataset("BCD-processed.txt",BCD_identities, 10)
-#for example in BCD_examples:
-#    print("Predicted class: " + BCD_identities[classify_example(example, BCD_learned)])
+##BCD_identities = [2,4]
+##BCD_examples = ["2,1,1,1,2,1,2,1,1,2".split(","),"10,10,10,4,8,1,8,10,1,4".split(","),"1,1,1,1,2,1,3,2,1,2".split(","),"5,1,3,1,2,1,2,1,1,2".split(",")]
+##BCD_learned = learn_dataset("BCD-processed.txt",BCD_identities, 10)
+##
+##Vote_identities = [0,1]
+##Vote_examples = ["0,1,0,1,1,1,0,0,0,1,0,1,1,1,0,1".split(","),"0,1,0,1,1,1,0,0,0,1,0,1,1,1,0,1".split(","),"1,0,1,0,0,0,1,1,1,1,0,0,0,0,1,0".split(","),"1,1,1,0,0,0,1,1,1,0,0,0,0,0,1,0".split(",")] 
+##Vote_learned = learn_dataset("house-votes_processed.csv",Vote_identities, 2)
+##
+##Soybean_identities = [1,2,3,4]
+##Soybean_examples =["4,0,2,1,1,1,0,1,0,2,1,1,0,2,2,0,0,0,1,0,3,1,1,1,0,0,0,0,4,0,0,0,0,0,0,1".split(","),"5,0,0,2,0,3,2,1,0,2,1,1,0,2,2,0,0,0,1,0,0,3,0,0,0,2,1,0,4,0,0,0,0,0,0,2".split(","),"0,1,2,0,0,1,1,1,1,1,1,0,0,2,2,0,0,0,1,0,1,1,0,1,1,0,0,3,4,0,0,0,0,0,0,3".split(","),"2,1,2,1,1,3,1,2,1,2,1,1,0,2,2,0,0,0,1,0,2,2,0,1,0,0,0,3,4,0,0,0,0,0,1,4".split(",")]
+##Soybean_learned = learn_dataset("soybean-preprocessed.csv",Soybean_identities, 6)
+##
+##for example in Soybean_examples:
+##    print("Soybean Predicted class: " + str(classify_example(example, Soybean_learned)))
+##    
+##for example in Vote_examples:
+##    print("Vote Predicted class: " + str(classify_example(example, Vote_learned)))
+##    
+##for example in BCD_examples:
+##    print("BCD Predicted class: " + str(BCD_identities[classify_example(example, BCD_learned)]))
+
